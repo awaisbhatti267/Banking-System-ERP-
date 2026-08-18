2fb228738239bb0f46e3462e5be261e880087436
Muhammad Awais Bhatti <66844710+awaisbhatti267@users.noreply.github.com>
2026-08-18 21:53:30 +0500

    feat: add dashboard page

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <main className="ml-60 pt-16 min-h-screen bg-slate-100 p-6">
+
+            <h1 className="text-2xl font-bold text-slate-800">
+                Dashboard
+            </h1>
+
+            <p className="text-slate-500 mt-1">
+                Welcome to Banking System ERP
+            </p>
+
+        </main>
+    )
+}
+
+export default Dashboard
